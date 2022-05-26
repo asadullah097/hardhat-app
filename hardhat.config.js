6cf9233ece9c707c80c8c1aa2184a5f3ca0e6e7c
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2022-05-27 00:22:04 +0500

    deployed contract to rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,15 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require('@nomiclabs/hardhat-waffle')
+
+const PRIVATE_KEY = process.env.KEY
 module.exports = {
   solidity: "0.7.3",
+
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  }
 };
